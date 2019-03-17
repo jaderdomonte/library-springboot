2e854cdd532d711d6d6c390c6dff68f943ccc1f2
Jader Augusto do Monte Santos <jaderdomonte@gmail.com>
2019-03-17 01:26:54 -0300

    Create Client Post

--- src/main/java/br/com/library/model/Author.java
+++ src/main/java/br/com/library/model/Author.java
@@ -8,13 +8,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Entity(name="author")
 public class Author extends AbstractEntity {
 
 	private static final long serialVersionUID = -5416252239364162615L;
+	
+	public Author() {}
+	
+	public Author(Long id, String name) {
+		super(id);
+		this.name = name;
+	}
 	
 	private String name;
 
--- src/main/java/br/com/library/rest/client/RestTemplateClient.java
+++ src/main/java/br/com/library/rest/client/RestTemplateClient.java
@@ -5,32 +5,101 @@ import java.util.List;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import br.com.library.model.Author;
 import br.com.library.model.Book;
 
 public class RestTemplateClient {
 	
 	private static final String URL = "http://localhost:8080/library/v1/protected/book";
+	private static final String URL_ADMIN = "http://localhost:8080/library/v1/admin/book";
+	private static RestTemplate restTemplateProtected;
+	private static RestTemplate restTemplateAdmin;
 	
 	public static void main(String[] args) {
-		RestTemplate restTemplate = new RestTemplateBuilder()
+		restTemplateProtected = new RestTemplateBuilder()
 										.rootUri(URL)
 										.basicAuthorization("jader", "senha")
 										.build();
-		Book book = restTemplate.getForObject("/{id}", Book.class, 1);
-		System.out.println(book);
-		
-		ResponseEntity<Book> forEntity = restTemplate.getForEntity("/{id}", Book.class, 1);
+		restTemplateAdmin = new RestTemplateBuilder()
+										.rootUri(URL_ADMIN)
+										.basicAuthorization("jader", "senha")
+										.build();
+//		getForObject();
+//		getForEntity();
+//		getExchange();
+//		getAllForObject();
+//		getAllForEntity();
+//		getAllExchange();
+		postForObject();
+		postForEntity();
+		postForExchange();
+	}
+
+	private static void getAllExchange() {
+		ResponseEntity<List<Book>> bookList = restTemplateProtected.exchange("/", HttpMethod.GET, null, new ParameterizedTypeReference<List<Book>>() {});
+		System.out.println(bookList.getBody());
+	}
+
+	private static void getAllForObject() {
+		Book[] books = restTemplateProtected.getForObject("/", Book[].class);
+		System.out.println(Arrays.toString(books));
+	}
+
+	private static void getForEntity() {
+		ResponseEntity<Book> forEntity = restTemplateProtected.getForEntity("/{id}", Book.class, 1);
 		System.out.println(forEntity);
 		System.out.println(forEntity.getBody());
+	}
+
+	private static void getForObject() {
+		Book book = restTemplateProtected.getForObject("/{id}", Book.class, 1);
+		System.out.println(book);
+	}
+	
+	private static void getExchange() {
+		ResponseEntity<List<Book>> bookList = restTemplateProtected.exchange("/", HttpMethod.GET, null, new ParameterizedTypeReference<List<Book>>() {});
+		System.out.println(bookList);
+	}
+	
+	private static void getAllForEntity() {
+		ResponseEntity<Book[]> forEntity = restTemplateProtected.getForEntity("/", Book[].class);
+		System.out.println(Arrays.asList(forEntity));
+	}
+	
+	private static Book postForObject() {
+		Book book = restTemplateAdmin.postForObject("/", createBook(), Book.class);
+		System.out.println(book);
+		return book;
+	}
+	
+	private static ResponseEntity<Book> postForEntity() {
+		ResponseEntity<Book> book = restTemplateAdmin.postForEntity("/", createBook(), Book.class);
+		System.out.println(book);
+		return book;
+	}
+	
+	private static ResponseEntity<Book> postForExchange(){
+		ResponseEntity<Book> exchange = restTemplateAdmin.exchange("/", HttpMethod.POST, new HttpEntity<>(createBook(), createHeaders()), Book.class);
+		System.out.println(exchange);
+		return exchange;
+	}
+	
+	private static Book createBook() {
+		return new Book("Livro 1", new Author(1L, "Author 1"));
+	}
+	
+	private static HttpHeaders createHeaders() {
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
 		
-		Book[] books = restTemplate.getForObject("/", Book[].class);
-		System.out.println(Arrays.toString(books));
-		
-		ResponseEntity<List<Book>> bookList = restTemplate.exchange("/", HttpMethod.GET, null, new ParameterizedTypeReference<List<Book>>() {});
-		System.out.println(bookList.getBody());
+		return httpHeaders;
 	}
 }
